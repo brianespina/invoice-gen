433fb0c613abfbcf24713b612f8334878b2e34e7
Brian Espina <espinabrian@gmail.com>
2024-08-15 16:56:59 +0800

    refactored to use my own list model

--- client/client.go
+++ client/client.go
@@ -1,45 +1,92 @@
 package client
 
 import (
+	"fmt"
+
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/huh"
+)
+
+type view int
+
+const (
+	normal view = iota
+	details
 )
 
 type Client struct {
-	Name string
+	name  string
+	email string
+	rate  float32
 }
-type ClientForm struct {
-	Form *huh.Form
+type ClientList struct {
+	list   []Client
+	cursor int
+	view   view
 }
 
-func NewCForm() ClientForm {
-	return ClientForm{
-		Form: huh.NewForm(
-			huh.NewGroup(
-				huh.NewInput().Key("name").Title("Client Name").Placeholder("John Doe"),
-			),
-		),
+func (l *ClientList) Populate() {
+	l.list = []Client{
+		{name: "White Sheep Digital", rate: 20, email: "sample@email.com"},
+		{name: "Marc Frojentein", rate: 8.5, email: "sample@email.com"},
+		{name: "Hicaliber", rate: 14, email: "sample@email.com"},
+	}
+}
+func New() ClientList {
+	return ClientList{
+		cursor: 0,
+	}
+}
+func (l ClientList) Init() tea.Cmd {
+	return nil
+}
+
+func (l ClientList) View() string {
+	switch l.view {
+	case details:
+		var s string
+		client := l.list[l.cursor]
+		s += fmt.Sprintf("%s\n", client.name)
+		s += fmt.Sprintf("%s\n", client.email)
+		s += fmt.Sprintf("%.2f\n", client.rate)
+		return s
+	case normal:
+		fallthrough
+	default:
+		var s string
+		for i, client := range l.list {
+			cursor := ""
+			if l.cursor == i {
+				cursor = ">"
+			}
+			s += fmt.Sprintf("%s %d %s\n", cursor, i, client.name)
+		}
+		return s
 	}
 }
 
-func (f ClientForm) Init() tea.Cmd {
-	return f.Form.Init()
-}
-
-func (f ClientForm) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-
-	var cmds []tea.Cmd
-	form, cmd := f.Form.Update(msg)
-	if ff, ok := form.(*huh.Form); ok {
-		f.Form = ff
-		cmds = append(cmds, cmd)
+func (l ClientList) Update(msg tea.Msg) (ClientList, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return l, tea.Quit
+		case "j":
+			if l.cursor == len(l.list)-1 {
+				l.cursor = 0
+			} else {
+				l.cursor++
+			}
+		case "k":
+			if l.cursor == 0 {
+				l.cursor = len(l.list) - 1
+			} else {
+				l.cursor--
+			}
+		case "d":
+			l.view = details
+		case "l":
+			l.view = normal
+		}
 	}
-	if f.Form.State == huh.StateCompleted {
-		cmds = append(cmds, tea.Quit)
-	}
-	return f, tea.Batch(cmds...)
-}
-
-func (f ClientForm) View() string {
-	return f.Form.View()
+	return l, nil
 }
--- main.go
+++ main.go
@@ -6,73 +6,81 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/huh"
 )
 
-type model struct {
-	clients       []client.Client
-	form          client.ClientForm
-	mode          string
-	clientName    string
-	clientContact string
+type mode int
+
+const (
+	clientView mode = iota
+	timeSheetView
+	clientAddView
+)
+
+type Client struct {
+	name, rate string
 }
 
+func (c Client) FilterValue() string {
+	return c.name
+}
+func (c Client) Title() string {
+	return c.name
+}
+func (c Client) Description() string {
+	return c.rate
+}
+
+type model struct {
+	list client.ClientList
+	mode mode
+}
+
+func (m *model) InitList() {
+	m.list = client.New()
+	m.list.Populate()
+}
 func (m model) Init() tea.Cmd {
-	if m.mode == "add" {
-		return m.form.Form.Init()
-	}
 	return nil
 }
-
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	if m.form.Form.State == huh.StateCompleted {
-		m.mode = "normal"
-		m.clients = append(m.clients, client.Client{Name: m.form.Form.GetString("name")})
-		m.form = client.NewCForm()
-		return m, nil
-	}
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.InitList()
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c":
 			return m, tea.Quit
-		case "ctrl+n":
-			m.mode = "add"
-			return m, nil
+		case "ctrl+t":
+			m.mode = timeSheetView
+		case "ctrl+a":
+			m.mode = clientAddView
+		case "esc":
+			m.mode = clientView
 		}
+	}
+	if m.mode == clientView {
+		cl, cmd := m.list.Update(msg)
+		m.list = cl
+		return m, cmd
 	}
 	return m, nil
 }
-
 func (m model) View() string {
 	switch m.mode {
-	case "add":
-		m.form.Form.Run()
-		return ""
-	case "normal":
-		s := ""
-		for _, client := range m.clients {
-			s += client.Name + "\n"
-		}
-		return s
+	case clientView:
+		return m.list.View()
+	case timeSheetView:
+		return "Timesheet View"
+	case clientAddView:
+		return "Add Clients"
 	}
-	return "something"
+	return m.list.View()
 }
-func initialModel() model {
-	m := model{
-		clients: []client.Client{
-			{
-				Name: "Brian Espina",
-			},
-		},
-		mode: "normal",
-		form: client.NewCForm(),
-	}
-	return m
+func New() *model {
+	return &model{}
 }
 func main() {
-	//p := tea.NewProgram(client.NewCForm())
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(New())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error has occurd: %v", err)
 		os.Exit(1)
